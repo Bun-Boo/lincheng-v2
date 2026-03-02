15aac90c5f645623dc6fa05d662b9c3e999102c5
hai.pt <haipt@boltholdings.com.vn>
2026-03-02 11:37:48 +0700

    refactor: externalize Firebase configuration to environment variables and ignore local environment file.

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCuA9pu1SeN9rnBc3_lZnAiX3Hsc3d9sts",
-  authDomain: "trang-1b214.firebaseapp.com",
-  projectId: "trang-1b214",
-  storageBucket: "trang-1b214.firebasestorage.app",
-  messagingSenderId: "569880433536",
-  appId: "1:569880433536:web:9d0adc2fc67f7cababa765"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
